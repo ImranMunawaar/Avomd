e69204c2c3ad968efaf555bc2108bbc4d46a5fd5
Imran <imran.munawar@synavos.com>
2026-02-09 13:51:29 +0500

    Add platform projects, assets, and configs

---
+++ android/app/src/columbiapsych/res/drawable/common/CommonImages.js
@@ -0,0 +1,13 @@
+const commonImages = {
+  abscess: require('./abscess.jpg'),
+  furuncle: require('./furuncle.jpg'),
+  carbuncle: require('./carbuncle.jpg'),
+  erysipelas: require('./erysipelas.jpg'),
+  epidermoid: require('./epidermoid.jpg'),
+  cellulitis: require('./cellulitis.jpg'),
+  ekghyperkalemia: require('./ekg_hyperkalemia.png'),
+  severitycoronakr_without_vs: require('./severitycoronakr_without_vs.png'),
+  severitycoronakr_with_vs: require('./severitycoronakr_with_vs.png'),
+};
+
+export default commonImages;
---
+++ ios/avoMD/AppDelegate.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+#import <Expo/Expo.h>
+#import <RCTAppDelegate.h>
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : EXAppDelegateWrapper
+
+@end
